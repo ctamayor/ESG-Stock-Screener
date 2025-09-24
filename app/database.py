a47c945555fc3cc928714fa1a25c38ae35a01035
ctamayor <camilo_tamayo-rousseau@brown.edu>
2025-09-24 12:29:09 -0400

    removed null pool

--- app/database.py
+++ app/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, NullPool
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
@@ -8,6 +8,6 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL,poolclass=NullPool)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
